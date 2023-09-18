9df6b560defa0a9c3797f4dc9a175962c01e46c4
mrkiedes <akiedes@vtsd.com>
2023-09-18 09:06:23 -0400

    Test File

---
+++ 1-1-2/step4.py
@@ -0,0 +1,8 @@
+name = input("What is your name?")
+address = input("What is your address?")
+city = input("What is your city?")
+state = input("What is your state?")
+zip = input("What is your zip?")
+print(name,"\n", city, ",", state, zip)
+
+print("My Favorite Quote is \"The answer to life, the universe, and everything is 42\". ")
