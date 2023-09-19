8e7f7bbc02732141240ff003ac94f3a477f2503f
mrkiedes <akiedes@vtsd.com>
2023-09-19 10:06:42 -0400

    Test File

---
+++ 1-1-1/turtleTest.py
@@ -0,0 +1,9 @@
+import turtle as trtl
+
+painter = trtl.Turtle()
+
+painter.forward(100)
+painter.right(90)
+
+wn = trtl.Screen()
+wn.mainloop()
---
+++ 1-1-2/castingPractice.py
@@ -0,0 +1,16 @@
+something = 1.999999
+somethingElse = 65
+um = "uh"
+
+print("Initial Values:")
+print("something:", something)
+print("somethingElse:", somethingElse)
+print("um:", um)
+
+somethingElse = chr(somethingElse)
+
+
+print("Ending Values:")
+print("something:", something)
+print("somethingElse:", somethingElse)
+print("um:", um)
