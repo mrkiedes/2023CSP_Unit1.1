672ae38b9198efd4d56557c3242f9eb628358dca
mrkiedes <akiedes@vtsd.com>
2023-09-28 08:02:42 -0400

    1.1.2 Complete

---
+++ 1-1-3/step1.py
@@ -0,0 +1,10 @@
+#   a113_forward_and_right.py
+import turtle as trtl
+
+painter = trtl.Turtle()
+
+# Add code to make the turtle move forward 20 pixels
+# and then turn right 20 degrees
+
+wn = trtl.Screen()
+wn.mainloop()
---
+++ 1-1-3/test.py
@@ -0,0 +1,3 @@
+for numbers in range(7):
+   print("Hello!")
+    print(numbers)
